8458d216c2e31d9d3ccd0770f773b9230b393380
Shunchao Wang <shunchao.wang@gmail.com>
2019-12-14 16:14:52 -0500

    initial import.

---
+++ src/main/java/me/thunder/springsocial/SpringSocialApplication.java
@@ -0,0 +1,13 @@
+package me.thunder.springsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialApplication.class, args);
+	}
+
+}
