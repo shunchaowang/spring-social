4953ca12ef5a001d4a6a72e7121761b29c50ee84
Shunchao Wang <s316255@aep.com>
2019-12-18 13:59:45 -0500

    Finished repository.

--- src/main/java/me/thunder/springsocial/repository/UserRepository.java
+++ src/main/java/me/thunder/springsocial/repository/UserRepository.java
@@ -5,7 +5,9 @@ import me.thunder.springsocial.model.User;
 import java.util.Optional;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
   Optional<User> findByEmail(String email);
