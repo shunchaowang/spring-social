f4de2b7733c81f200acf0beac7fb35066765d404
Shunchao Wang <s316255@aep.com>
2020-02-20 14:19:49 -0500

    Added cookie repository.

--- src/main/java/me/thunder/springsocial/security/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/me/thunder/springsocial/security/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -3,9 +3,13 @@ package me.thunder.springsocial.security.oauth2;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.nimbusds.oauth2.sdk.util.StringUtils;
+
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.stereotype.Component;
+
+import me.thunder.springsocial.util.CookieUtils;
 
 @Component
 public class HttpCookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository {
@@ -16,20 +20,36 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
 
   @Override
   public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
-    return null;
+    return CookieUtils.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
+        .map(cookie -> CookieUtils.deserialize(cookie, OAuth2AuthorizationRequest.class)).orElse(null);
   }
 
   @Override
   public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request) {
-    // TODO Auto-generated method stub
-    return null;
+    return this.loadAuthorizationRequest(request);
   }
 
   @Override
   public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request,
       HttpServletResponse response) {
-    // TODO Auto-generated method stub
 
+    if (authorizationRequest == null) {
+      CookieUtils.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+      CookieUtils.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
+      return;
+    }
+
+    CookieUtils.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME,
+        CookieUtils.serialize(authorizationRequest), cookieExpireSeconds);
+
+    String redirectUriAfterLogin = request.getParameter(REDIRECT_URI_PARAM_COOKIE_NAME);
+    if (StringUtils.isNotBlank(redirectUriAfterLogin)) {
+      CookieUtils.addCookie(response, REDIRECT_URI_PARAM_COOKIE_NAME, redirectUriAfterLogin, cookieExpireSeconds);
+    }
   }
 
+  public void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
+    CookieUtils.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+    CookieUtils.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
+  }
 }
