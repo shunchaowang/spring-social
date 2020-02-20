639f140f72da7f9c4b4785b18daadc0b7ed9068d
Shunchao Wang <s316255@aep.com>
2020-02-20 13:59:51 -0500

    Added cookie utils.

--- src/main/java/me/thunder/springsocial/config/SecurityConfig.java
+++ src/main/java/me/thunder/springsocial/config/SecurityConfig.java
@@ -1,13 +1,26 @@
 package me.thunder.springsocial.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import me.thunder.springsocial.security.CustomUserDetailsService;
+import me.thunder.springsocial.security.oauth2.CustomOAuth2UserService;
+import me.thunder.springsocial.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  private CustomUserDetailsService customUserDetailsService;
+
+  @Autowired
+  private CustomOAuth2UserService customOAuth2UserService;
+
+  @Autowired
+  private HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 }
--- src/main/java/me/thunder/springsocial/security/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/me/thunder/springsocial/security/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class HttpCookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository {
 
+  public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+  public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
+  private static final int cookieExpireSeconds = 180;
+
   @Override
   public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
-    // TODO Auto-generated method stub
     return null;
   }
 
