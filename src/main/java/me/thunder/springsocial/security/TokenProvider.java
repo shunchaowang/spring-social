4ef6be61a50b0f3085cb4d951b57bb123b358937
Shunchao Wang <s316255@aep.com>
2020-03-10 14:10:17 -0400

    Wip.

--- src/main/java/me/thunder/springsocial/security/TokenProvider.java
+++ src/main/java/me/thunder/springsocial/security/TokenProvider.java
@@ -1,17 +1,22 @@
 package me.thunder.springsocial.security;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import me.thunder.springsocial.config.AppProperties;
 
 @Service
 public class TokenProvider {
 
   private static final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
-  private final AppProperties appProperties;
+  private AppProperties appProperties;
 
   public TokenProvider(AppProperties appProperties) {
     this.appProperties = appProperties;
@@ -19,7 +24,17 @@ public class TokenProvider {
 
   public String createToken(Authentication authentication) {
     UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+    Date now = new Date();
+    Date expiryDate = new Date(now.getTime() + appProperties.getAuth().getTokenExpirationMsec());
+    return Jwts.builder().setSubject(Long.toString(userPrincipal.getId())).setIssuedAt(new Date())
+        .setExpiration(expiryDate).signWith(SignatureAlgorithm.HS512, appProperties.getAuth().getTokenSecret())
+        .compact();
+  }
 
-    return null;
+  public Long getUserIdFromToken(String token) {
+    Claims claims = Jwts.parser().setSigningKey(appProperties.getAuth().getTokenSecret()).parseClaimsJws(token)
+        .getBody();
+
+    return Long.parseLong(claims.getSubject());
   }
 }
