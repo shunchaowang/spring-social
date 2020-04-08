0887f0bc4d789665ea6f51a35e5b13277071f38b
Shunchao Wang <shunchao.wang@gmail.com>
2020-04-08 10:09:56 -0400

    wip.

--- src/main/java/me/thunder/springsocial/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/me/thunder/springsocial/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -1,6 +1,7 @@
 package me.thunder.springsocial.security.oauth2;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Optional;
 
 import javax.servlet.ServletException;
@@ -12,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import me.thunder.springsocial.config.AppProperties;
+import me.thunder.springsocial.exception.BadRequestException;
 import me.thunder.springsocial.security.TokenProvider;
 import me.thunder.springsocial.util.CookieUtils;
 
@@ -43,7 +46,28 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
   protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
       Authentication authentication) {
     Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME).map(Cookie::getValue);
+    if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+      throw new BadRequestException(
+          "Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
+    }
+    String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
 
-    return null;
+    String token = tokenProvider.createToken(authentication);
+
+    return UriComponentsBuilder.fromUriString(targetUrl).queryParam("token", token).build().toUriString();
+  }
+
+  private boolean isAuthorizedRedirectUri(String uri) {
+    URI clientRedirectUri = URI.create(uri);
+    return appProperties.getOauth2().getAuthorizedRedirectUris().stream().anyMatch(authorizedRedirectUri -> {
+      // Only validate host and port. Let the clients use different paths if they want
+      // to
+      URI authorizedURI = URI.create(authorizedRedirectUri);
+      if (authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+          && authorizedURI.getPort() == clientRedirectUri.getPort()) {
+        return true;
+      }
+      return false;
+    });
   }
 }
