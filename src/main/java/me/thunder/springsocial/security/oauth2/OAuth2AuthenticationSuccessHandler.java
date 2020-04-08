6bff9940cd29b5faf3409c216dc0efd74eafb31f
Shunchao Wang <shunchao.wang@gmail.com>
2020-04-08 08:21:45 -0400

    Set up env on my windows 10.

--- src/main/java/me/thunder/springsocial/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/me/thunder/springsocial/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -37,7 +37,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
       Authentication authentication) throws IOException, ServletException {
 
-        String targetUrl = 
+    String targetUrl;
   }
 
   protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
