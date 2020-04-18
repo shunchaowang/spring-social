738e465043990fffeb91f6d263f73e55baba657b
Shunchao Wang <shunchao.wang@gmail.com>
2020-04-18 13:36:17 -0400

    Finished TokenAuthenticationFilter.

--- src/main/java/me/thunder/springsocial/security/CustomUserDetailsService.java
+++ src/main/java/me/thunder/springsocial/security/CustomUserDetailsService.java
@@ -1,17 +1,34 @@
 package me.thunder.springsocial.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import me.thunder.springsocial.exception.ResourceNotFoundException;
+import me.thunder.springsocial.model.User;
+import me.thunder.springsocial.repository.UserRepository;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+  @Autowired
+  private UserRepository userRepository;
+
   @Override
+  @Transactional
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    // TODO Auto-generated method stub
-    return null;
+    User user = userRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+    return UserPrincipal.create(user);
   }
 
+  @Transactional
+  public UserDetails loadUserById(Long id) {
+    User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+
+    return UserPrincipal.create(user);
+  }
 }
